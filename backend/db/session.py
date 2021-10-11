d0513f661010ed4a946661cd74d997479366b927
mamichalowski <m_michalowski@interia.pl>
2021-10-12 00:55:49 +0200

    Commit with unittest

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,9 +1,25 @@
+from typing import Generator
 from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
+from typing import Generator
 
-SQLALCHEMY_DATABASE = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE)
+from core.config import settings
+
+#SQLALCHEMY_DATABASE = settings.DATABASE_URL
+#engine = create_engine(SQLALCHEMY_DATABASE)
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit = False,autoflush = False, bind=engine)
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,35 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base import Base
+from apis.base import api_router
+import uvicorn
 
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
+def include_router(app):
+    app.include_router(api_router)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    include_router(app)
+    return app
+
+
+
+
+
+
+
+app = start_application()
 
 @app.get("/")
 def hello_app():
     return {"detail":"Hello World !"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
